c0650c4de105b27da451085b05b3ec222a67def6
JanuszPXYZ <19962689+JanuszPXYZ@users.noreply.github.com>
2021-02-21 01:21:45 +0100

    Change in one of the functions

--- StooqDownloadPrepare.py
+++ StooqDownloadPrepare.py
@@ -3,11 +3,13 @@ from gazpacho import get, Soup
 import re
 import urllib
 
-
+# Example Query
 #url_query = "http://stooq.pl/q/d/"
 #csv_download_query = "l/?s=mwig40&i=d"
 #full_query = url_query + csv_download_query
 
+# ToDo: Ideally, all of those functions could be stored in one class, where
+# one of the parameters passed to __init__() would be the query for the desired index.
 
 def get_index_components(query: str = 'https://stooq.pl/q/i/?s=mwig40'):
     '''
@@ -53,14 +55,22 @@ def download_ticker_data(ticker: str, sep: str = ','):
     return ticker_data
 
 
-def closing_prices_for_index_components(tickers: list):
+def prices_for_index_components(tickers: list, column: str = "Zamkniecie"):
     '''
-    Returns a full dataset containing the closing prices for the companies within
-    an index.
+    Returns a full dataset containing the prices for specified column for the companies within
+    a given index.
+
+    Args:
+
+    tickers: a list of tickers acquired through get_index_components or other list containing names
+    of companies
+
+    column: a string specifying the desired column to be displayed for all of the companies.
+    The default parameter for this argument  is "Zamkniecie" (Closing price).
     '''
     companies_closing_price = []
     for company in tickers:
-        companies_closing_price.append(download_ticker_data(company)['Zamkniecie'])
+        companies_closing_price.append(download_ticker_data(company)[column])
 
     full_df = pd.concat(companies_closing_price, axis = 1, sort = True)
     full_df.columns = tickers
